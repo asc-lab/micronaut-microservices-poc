c4187e3af11275b3a0b56c85ca24a00873e54943
Robert Witkowski <robert.witkowski@altkom.pl>
2018-07-26 15:10:31 +0200

    Add example api jar.

---
+++ api/src/main/java/pl/altkom/asc/lab/micronaut/poc/api/v1/PolicyDto.java
@@ -0,0 +1,4 @@
+package pl.altkom.asc.lab.micronaut.poc.api.v1;
+
+public class PolicyDto {
+}
---
+++ api/src/main/java/pl/altkom/asc/lab/micronaut/poc/api/v1/PolicyOperations.java
@@ -0,0 +1,8 @@
+package pl.altkom.asc.lab.micronaut.poc.api.v1;
+
+import io.micronaut.validation.Validated;
+
+@Validated
+public class PolicyOperations<P extends PolicyDto> {
+
+}
