64548d7f804061f55241f1cc11af75119b67ff72
wojtek.suwala <wojtek.suwala@outlook.com>
2018-07-31 08:11:11 +0200

    products data loader fix

--- product-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/product/service/init/DataLoader.java
+++ product-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/product/service/init/DataLoader.java
@@ -3,9 +3,11 @@ package pl.altkom.asc.lab.micronaut.poc.product.service.init;
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.runtime.server.event.ServerStartupEvent;
 import lombok.RequiredArgsConstructor;
+import pl.altkom.asc.lab.micronaut.poc.product.service.domain.Product;
 import pl.altkom.asc.lab.micronaut.poc.product.service.infrastructure.ProductsRepository;
 
 import javax.inject.Singleton;
+import java.util.List;
 
 @Singleton
 @RequiredArgsConstructor
@@ -15,9 +17,22 @@ public class DataLoader implements ApplicationEventListener<ServerStartupEvent> 
 
     @Override
     public void onApplicationEvent(ServerStartupEvent serverStartupEvent) {
-            productsRepository.add(DemoProductsFactory.car());
-            productsRepository.add(DemoProductsFactory.farm());
-            productsRepository.add(DemoProductsFactory.house());
-            productsRepository.add(DemoProductsFactory.travel());
+        List<Product> allProducts = productsRepository.findAll().blockingGet();
+
+        if (allProducts.stream().noneMatch(p -> p.getCode().equals("CAR"))) {
+            productsRepository.add(DemoProductsFactory.car()).blockingGet();
+        }
+
+        if (allProducts.stream().noneMatch(p -> p.getCode().equals("FAI"))) {
+            productsRepository.add(DemoProductsFactory.farm()).blockingGet();
+        }
+
+        if (allProducts.stream().noneMatch(p -> p.getCode().equals("HSI"))) {
+            productsRepository.add(DemoProductsFactory.house()).blockingGet();
+        }
+
+        if (allProducts.stream().noneMatch(p -> p.getCode().equals("TRI"))) {
+            productsRepository.add(DemoProductsFactory.travel()).blockingGet();
+        }
     }
 }
