1587661335fad81eea42b4c4a2c6f4574a053ccc
wojteksuwala <wojtek.suwala@outlook.com>
2018-08-03 15:29:27 +0200

    build fix

--- agent-portal-gateway/src/main/java/pl/altkom/asc/lab/micronaut/poc/gateway/client/v1/fallback/PaymentGatewayClientFallback.java
+++ agent-portal-gateway/src/main/java/pl/altkom/asc/lab/micronaut/poc/gateway/client/v1/fallback/PaymentGatewayClientFallback.java
@@ -7,6 +7,7 @@ import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.PolicyAccountDto;
 import javax.inject.Singleton;
 import java.util.Collection;
 import java.util.Collections;
+import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.PolicyAccountBalanceDto;
 
 @Singleton
 @Fallback
@@ -14,5 +15,10 @@ public class PaymentGatewayClientFallback implements PaymentGatewayClient {
     @Override
     public Collection<PolicyAccountDto> accounts() {
         return Collections.emptyList();
+    }
+
+    @Override
+    public PolicyAccountBalanceDto accountBalance(String accountNumber) {
+        return new PolicyAccountBalanceDto(accountNumber,null,null);
     }
 }
