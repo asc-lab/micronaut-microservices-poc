e9dccaffb135aae565f5eb093f36026f495d6636
Robert Kusmierek <robert.kusmierek@altkom.pl>
2018-08-07 17:09:10 +0200

    simple build script (without creating containers)

---
+++ build.sh
@@ -0,0 +1,34 @@
+mvn clean install -f policy-service-api
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f pricing-service-api
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f product-service-api
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f auth-service
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f payment-service
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f policy-service -Dmaven.test.skip
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f pricing-service -Dmaven.test.skip
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f product-service
+[ $? -eq 0 ] || exit 1
+
+mvn clean install -f agent-portal-gateway
+[ $? -eq 0 ] || exit 1
+
+(
+  cd web-vue
+  yarn install
+  yarn run build
+)
+[ $? -eq 0 ] || exit 1
+
