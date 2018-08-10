f4bbf65061311efb71593f0d9ca1ea384806592e
Robert Witkowski <robert.witkowski@altkom.pl>
2018-08-10 10:15:58 +0200

    Add command-bus module. Move policy search to separate service #12

--- policy-service-api/src/main/java/pl/altkom/asc/lab/micronaut/poc/policy/service/api/v1/commands/Command.java
+++
@@ -1,4 +0,0 @@
-package pl.altkom.asc.lab.micronaut.poc.policy.service.api.v1.commands;
-
-public interface Command<R> {
-}
--- policy-service-api/src/main/java/pl/altkom/asc/lab/micronaut/poc/policy/service/api/v1/queries/Query.java
+++
@@ -1,4 +0,0 @@
-package pl.altkom.asc.lab.micronaut.poc.policy.service.api.v1.queries;
-
-public interface Query<R> {
-}
