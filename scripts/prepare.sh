094ef8f79e20d2c0b1d9442b196334ad0a027146
Robert Kusmierek <robert.kusmierek@altkom.pl>
2018-09-10 17:54:33 +0200

    create empty policy template

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,3 +4,8 @@ sed -i -E 's|^(127.0.0.1.*)$|\1 kafkaserver|' /etc/hosts
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8081 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8090 -j ACCEPT
+
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d '{"name": "POLICY", "recipe" : "chrome-pdf", "engine": "handlebars", "content": "POLICY"}' \
+    localhost:5488/odata/templates
