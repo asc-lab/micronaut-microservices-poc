06d1bbf2e1c0685d0f70a5cd5269afb37039ee5e
Robert Witkowski <robert.witkowski@altkom.pl>
2018-09-12 08:18:12 +0200

    Fix missing data in POST request.

--- web-vue/src/components/http/ApiClient.js
+++ web-vue/src/components/http/ApiClient.js
@@ -3,21 +3,19 @@ import auth from './Auth';
 
 export const HTTP = axios.create({
     baseURL: process.env.VUE_APP_BACKEND_URL,
-    transformRequest: [
-        (data, headers) => {
-            headers.Authorization = auth.getAuthHeader()
-        }
-    ]
+    headers: {
+        'Authorization': auth.getAuthHeader()
+    }
 });
 
 HTTP.interceptors.response.use(
     (response) => {
-        return response
+        return response;
     },
     (error) => {
         console.log(error.response.status);
         if (error.response.status === 401 || error.response.status === 403) {
-            window.location.href = '/#/account'
+            window.location.href = '/#/account';
         }
     }
 );
