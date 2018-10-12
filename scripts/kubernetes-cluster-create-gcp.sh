8295b04afc2aacb8a7e902830abef22308fe412d
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2018-10-12 18:52:01 +0200

    scripts for provisioning k8s cluster on gcp and azure

---
+++ scripts/kubernetes-cluster-create-gcp.sh
@@ -0,0 +1,1 @@
+gcloud container clusters create micronaut --num-nodes 4 --zone europe-west3-a --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
---
+++ scripts/kubernetes-cluster-delete-gcp.sh
@@ -0,0 +1,1 @@
+gcloud container clusters delete micronaut --zone europe-west3-a
