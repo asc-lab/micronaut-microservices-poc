c2df4f22a61e6cb2046968565ba692f182c9b7f9
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2018-10-15 13:55:42 +0200

    upgrade kafka to latest version

--- kafka-create-cluster.sh
+++ kafka-create-cluster.sh
@@ -1,7 +1,11 @@
+KAFKAMANAGER=kafka-manager:9000
 CLUSTER_NAME=kafka-cluster
+ZK_HOST=zookeeper
+ZK_PORT=2181
+KAFKA_VERSION=1.1.0
 curl \
   -vs -o /dev/null \
-  -d "name=${CLUSTER_NAME}&zkHosts=kafkaserver%3A2181&kafkaVersion=0.9.0.1&jmxUser=&jmxPass=&pollConsumers=true&tuning.brokerViewUpdatePeriodSeconds=30&tuning.clusterManagerThreadPoolSize=2&tuning.clusterManagerThreadPoolQueueSize=100&tuning.kafkaCommandThreadPoolSize=2&tuning.kafkaCommandThreadPoolQueueSize=100&tuning.logkafkaCommandThreadPoolSize=2&tuning.logkafkaCommandThreadPoolQueueSize=100&tuning.logkafkaUpdatePeriodSeconds=30&tuning.partitionOffsetCacheTimeoutSecs=5&tuning.brokerViewThreadPoolSize=3&tuning.brokerViewThreadPoolQueueSize=1000&tuning.offsetCacheThreadPoolSize=3&tuning.offsetCacheThreadPoolQueueSize=1000&tuning.kafkaAdminClientThreadPoolSize=3&tuning.kafkaAdminClientThreadPoolQueueSize=1000" \
+  -d "name=${CLUSTER_NAME}&zkHosts=${ZK_HOST}%3A${ZK_PORT}&kafkaVersion=${KAFKA_VERSION}&jmxUser=&jmxPass=&tuning.brokerViewUpdatePeriodSeconds=30&tuning.clusterManagerThreadPoolSize=2&tuning.clusterManagerThreadPoolQueueSize=100&tuning.kafkaCommandThreadPoolSize=2&tuning.kafkaCommandThreadPoolQueueSize=100&tuning.logkafkaCommandThreadPoolSize=2&tuning.logkafkaCommandThreadPoolQueueSize=100&tuning.logkafkaUpdatePeriodSeconds=30&tuning.partitionOffsetCacheTimeoutSecs=5&tuning.brokerViewThreadPoolSize=8&tuning.brokerViewThreadPoolQueueSize=1000&tuning.offsetCacheThreadPoolSize=8&tuning.offsetCacheThreadPoolQueueSize=1000&tuning.kafkaAdminClientThreadPoolSize=8&tuning.kafkaAdminClientThreadPoolQueueSize=1000&tuning.kafkaManagedOffsetMetadataCheckMillis=30000&tuning.kafkaManagedOffsetGroupCacheSize=1000000&tuning.kafkaManagedOffsetGroupExpireDays=7&securityProtocol=PLAINTEXT" \
   -X POST \
-  http://localhost:9000/clusters 2>&1
+  http://${KAFKAMANAGER}/clusters 2>&1
 
