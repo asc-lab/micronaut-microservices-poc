81d1edb3151d05f600e3780c02f312aad155bdfe
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2018-10-15 17:18:41 +0200

    smarter k8s deployment and support for dependencies

--- scripts/kubernetes-deploy.sh
+++ scripts/kubernetes-deploy.sh
@@ -3,11 +3,26 @@ BASE=`dirname $0`
 #required by elasticsearch
 #sysctl -w vm.max_map_count=262144
 
+wait-for-deployment() {
+    DEPLOYMENT=$1
+    REPLICAS=0
+    until [ $(kubectl get deployments -o json |jq -c ".items[] | select(.status.readyReplicas == 1 and .metadata.name == \"${DEPLOYMENT}\")" | jq .metadata.name | wc -l) -gt 0 ]; do
+      echo waiting
+      sleep 2
+    done
+}
+
 SERVICES=`find ${BASE}/../*/k8s/*service.yml`
 for SRV in ${SERVICES} ;
 do
     kubectl apply -f ${SRV}
 done
+
+kubectl apply -f ${BASE}/../infra/k8s/zookeeper-deployment.yml
+echo "waiting for zookeeper to start"
+wait-for-deployment zookeeper
+# workaround for missing zk readiness probe
+sleep 5
 
 DEPLOYMENTS=`find ${BASE}/../infra/k8s/*deployment.yml`
 for DEPLOYMENT in ${DEPLOYMENTS} ;
@@ -15,8 +30,10 @@ do
     kubectl apply -f ${DEPLOYMENT}
 done
 
-echo "waiting for infra to start"
-sleep 10
+echo "waiting for kafka-manager to start"
+wait-for-deployment kafka-manager
+echo "waiting for kafka-server to start"
+wait-for-deployment kafka-server
 
 ${BASE}/kafka-create-cluster.sh
 
