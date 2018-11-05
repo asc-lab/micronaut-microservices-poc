c0d4c31c928f6538806a75d02810348adeacdded
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2018-11-05 17:28:04 +0100

    fixed typo

--- scripts/kubernetes-cluster-create-azure.sh
+++ scripts/kubernetes-cluster-create-azure.sh
@@ -1,5 +1,5 @@
 RESOURCE_GROUP=micronaut
 CLUSTER=micronaut
 az group create --name ${RESOURCE_GROUP} --location westeurope
-az acs create --orchestrator-type kubernetes --agent-count 2 ---agent-vm-size Standard_D2_v2 --resource-group ${RESOURCE_GROUP} --name ${CLUSTER} --generate-ssh-keys
+az acs create --orchestrator-type kubernetes --agent-count 2 --agent-vm-size Standard_D2_v2 --resource-group ${RESOURCE_GROUP} --name ${CLUSTER} --generate-ssh-keys
 az acs kubernetes get-credentials --resource-group=${RESOURCE_GROUP} --name=${CLUSTER}
