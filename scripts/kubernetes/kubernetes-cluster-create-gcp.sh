1be82f900e7df3c7e49f51e1b415bccff10e1c1f
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2018-11-06 19:04:10 +0100

    make machine type for k8s on gcp explicite

--- scripts/kubernetes/kubernetes-cluster-create-gcp.sh
+++ scripts/kubernetes/kubernetes-cluster-create-gcp.sh
@@ -1,1 +1,1 @@
-gcloud container clusters create micronaut --num-nodes 4 --zone europe-west3-a --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+gcloud container clusters create micronaut --machine-type n1-standard-1 --num-nodes 4 --zone europe-west3-a --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
