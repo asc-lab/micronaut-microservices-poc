9e00ec96bca7744f5f6b01129403ca1ded484fb5
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2018-11-13 16:54:13 +0100

    change azure kubernetes service: acs -> aks

--- scripts/kubernetes/kubernetes-cluster-create-azure.sh
+++ scripts/kubernetes/kubernetes-cluster-create-azure.sh
@@ -1,5 +1,14 @@
 RESOURCE_GROUP=micronaut
 CLUSTER=micronaut
 az group create --name ${RESOURCE_GROUP} --location westeurope
-az acs create --orchestrator-type kubernetes --agent-count 2 --agent-vm-size Standard_D2_v2 --resource-group ${RESOURCE_GROUP} --name ${CLUSTER} --generate-ssh-keys
-az acs kubernetes get-credentials --resource-group=${RESOURCE_GROUP} --name=${CLUSTER} --overwrite-existing
+az aks create \
+    --resource-group ${RESOURCE_GROUP} \
+    --name ${CLUSTER} \
+    --node-vm-size Standard_D2_v2 \
+    --node-count 2 \
+    --enable-addons monitoring \
+    --generate-ssh-keys
+az aks get-credentials \
+    --resource-group=${RESOURCE_GROUP} \
+    --name=${CLUSTER} \
+    --overwrite-existing
