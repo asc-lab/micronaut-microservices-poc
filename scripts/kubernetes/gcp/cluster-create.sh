13aaf714d5e0a782d7dbaaa29dcc80c9354a1670
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-01-17 17:39:13 +0100

    set specific kubernetes version (on gcp)

--- scripts/kubernetes/gcp/cluster-create.sh
+++ scripts/kubernetes/gcp/cluster-create.sh
@@ -1,5 +1,6 @@
 gcloud container clusters create micronaut \
     --machine-type n1-standard-2 \
     --num-nodes 2 \
+    --cluster-version 1.11.6-gke.2 \
     --zone europe-west3-a \
     --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
