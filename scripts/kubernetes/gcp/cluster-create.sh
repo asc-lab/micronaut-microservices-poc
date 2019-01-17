eaf5840f43459f20e57304f8d9a1195d25f2b832
Robert Kuśmierek <robert.kusmierek@altkom.pl>
2019-01-17 16:54:32 +0100

    improve script readability

--- scripts/kubernetes/gcp/cluster-create.sh
+++ scripts/kubernetes/gcp/cluster-create.sh
@@ -1,1 +1,5 @@
-gcloud container clusters create micronaut --machine-type n1-standard-2 --num-nodes 2 --zone europe-west3-a --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
+gcloud container clusters create micronaut \
+    --machine-type n1-standard-2 \
+    --num-nodes 2 \
+    --zone europe-west3-a \
+    --scopes "https://www.googleapis.com/auth/projecthosting,storage-rw"
