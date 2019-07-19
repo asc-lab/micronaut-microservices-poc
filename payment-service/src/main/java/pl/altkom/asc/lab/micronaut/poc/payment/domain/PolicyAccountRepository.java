67377030cc4fbc89069cf0a72d98e31a93fd35bb
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2019-07-19 21:59:37 +0200

    PaymentService: fix lazy loading issue by adding entityGraph annotation

--- payment-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/payment/domain/PolicyAccountRepository.java
+++ payment-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/payment/domain/PolicyAccountRepository.java
@@ -5,15 +5,16 @@ import java.util.Optional;
 
 import io.micronaut.data.repository.GenericRepository;
 import io.micronaut.data.annotation.*;
+import io.micronaut.data.jpa.annotation.EntityGraph;
 import io.micronaut.data.model.*;
 
 @Repository
 public interface PolicyAccountRepository extends GenericRepository<PolicyAccount, Long> {
 
-    @Join(value = "entries", type = Join.Type.FETCH) 
+    @EntityGraph(attributePaths = {"entries"})  
     Optional<PolicyAccount> findByPolicyNumber(String policyNumber);
     
-    @Join(value = "entries", type = Join.Type.FETCH) 
+    @EntityGraph(attributePaths = {"entries"}) 
     @Query("FROM PolicyAccount p WHERE p.policyAccountNumber = :policyAccountNumber")
     Optional<PolicyAccount> findByPolicyAccountNumber(String policyAccountNumber);
 
