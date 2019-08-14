2cf7ebccdd22a869375b29599a32889013a4c646
Robert Witkowski <robert.witkowski@altkomsoftware.pl>
2019-08-14 09:59:13 +0200

    Remove unused imports.

--- payment-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/payment/infrastructure/adapters/web/PaymentController.java
+++ payment-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/payment/infrastructure/adapters/web/PaymentController.java
@@ -2,16 +2,15 @@ package pl.altkom.asc.lab.micronaut.poc.payment.infrastructure.adapters.web;
 
 import io.micronaut.configuration.hystrix.annotation.HystrixCommand;
 import io.micronaut.http.annotation.Controller;
-import java.time.LocalDate;
-import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.PolicyAccountDto;
-import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.operations.PaymentOperations;
-import pl.altkom.asc.lab.micronaut.poc.payment.domain.PolicyAccountRepository;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import pl.altkom.asc.lab.micronaut.poc.payment.domain.PolicyAccountRepository;
 import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.PolicyAccountBalanceDto;
+import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.PolicyAccountDto;
 import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.exceptions.PolicyAccountNotFound;
+import pl.altkom.asc.lab.micronaut.poc.payment.service.api.v1.operations.PaymentOperations;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 @Controller("/payment")
 @RequiredArgsConstructor
@@ -24,7 +23,7 @@ public class PaymentController implements PaymentOperations {
     public Collection<PolicyAccountDto> accounts() {
         return policyAccountRepository.findAll();
     }
-    
+
     @Override
     @HystrixCommand
     public PolicyAccountBalanceDto accountBalance(String accountNumber) {
