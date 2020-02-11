26404f117b6f0e199080a8b7d0eae4be61be6ab0
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2020-02-11 13:01:29 +0100

    script fix

--- build-without-tests.sh
+++ build-without-tests.sh
@@ -55,9 +55,6 @@ mvn clean install -f dashboard-service -DskipTests
 mvn clean install -f agent-portal-gateway -DskipTests
 [ $? -eq 0 ] || exit 1
 
-npm install -g n
-[ $? -eq 0 ] || exit 1
-
 n 10.16.3
 [ $? -eq 0 ] || exit 1
 
