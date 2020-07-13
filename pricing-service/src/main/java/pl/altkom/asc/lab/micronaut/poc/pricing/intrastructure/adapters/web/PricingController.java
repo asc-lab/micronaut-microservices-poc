aed1e59a0cb508abf07eabfdb8b718c144b02521
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2020-07-13 20:40:22 +0200

    upgrade pricing-service to micronaut 2.0

--- pricing-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/pricing/intrastructure/adapters/web/PricingController.java
+++ pricing-service/src/main/java/pl/altkom/asc/lab/micronaut/poc/pricing/intrastructure/adapters/web/PricingController.java
@@ -1,13 +1,16 @@
 package pl.altkom.asc.lab.micronaut.poc.pricing.intrastructure.adapters.web;
 
+import pl.altkom.asc.lab.micronaut.poc.pricing.commands.CalculatePriceHandler;
+import pl.altkom.asc.lab.micronaut.poc.pricing.service.api.v1.PricingOperations;
+import pl.altkom.asc.lab.micronaut.poc.pricing.service.api.v1.commands.calculateprice.CalculatePriceCommand;
+import pl.altkom.asc.lab.micronaut.poc.pricing.service.api.v1.commands.calculateprice.CalculatePriceResult;
+
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 import io.micronaut.validation.Validated;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import pl.altkom.asc.lab.micronaut.poc.pricing.commands.CalculatePriceHandler;
-import pl.altkom.asc.lab.micronaut.poc.pricing.service.api.v1.commands.calculateprice.CalculatePriceCommand;
-import pl.altkom.asc.lab.micronaut.poc.pricing.service.api.v1.commands.calculateprice.CalculatePriceResult;
-import pl.altkom.asc.lab.micronaut.poc.pricing.service.api.v1.PricingOperations;
 
 @Controller("/pricing")
 @Validated
@@ -17,6 +20,7 @@ public class PricingController implements PricingOperations {
 
     private final CalculatePriceHandler calculatePriceHandler;
 
+    @ExecuteOn(TaskExecutors.IO)
     @Override
     public CalculatePriceResult calculatePrice(CalculatePriceCommand cmd) {
         return calculatePriceHandler.handle(cmd);
