6835acd90b869786cc59d3663cfb311d02683150
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2020-07-17 17:13:44 +0200

    upgrade document-service to micronaut 2.0

--- documents-service/src/main/kotlin/pl/altkom/asc/lab/micronaut/poc/documents/infrastructure/adapters/db/PolicyDocumentDb.kt
+++ documents-service/src/main/kotlin/pl/altkom/asc/lab/micronaut/poc/documents/infrastructure/adapters/db/PolicyDocumentDb.kt
@@ -1,18 +1,16 @@
 package pl.altkom.asc.lab.micronaut.poc.documents.infrastructure.adapters.db
 
-import pl.altkom.asc.lab.micronaut.poc.documents.infrastructure.annotations.RequiresJdbc
 import pl.altkom.asc.lab.micronaut.poc.documents.domain.PolicyDocument
 import pl.altkom.asc.lab.micronaut.poc.documents.domain.PolicyDocumentRepository
-import io.micronaut.configuration.hibernate.jpa.scope.CurrentSession
-import io.micronaut.spring.tx.annotation.Transactional
+import pl.altkom.asc.lab.micronaut.poc.documents.infrastructure.annotations.RequiresJdbc
 import javax.inject.Singleton
 import javax.persistence.EntityManager
+import javax.transaction.Transactional
 import kotlin.streams.toList
 
 @Singleton
 @RequiresJdbc
 open class PolicyDocumentDb(
-        @CurrentSession
         private val entityManager: EntityManager
 ) : PolicyDocumentRepository {
 
--- documents-service/src/main/kotlin/pl/altkom/asc/lab/micronaut/poc/documents/infrastructure/adapters/web/DocumentsController.kt
+++ documents-service/src/main/kotlin/pl/altkom/asc/lab/micronaut/poc/documents/infrastructure/adapters/web/DocumentsController.kt
@@ -1,12 +1,15 @@
 package pl.altkom.asc.lab.micronaut.poc.documents.infrastructure.adapters.web
 
+import io.micronaut.http.annotation.Controller
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
+import io.micronaut.validation.Validated
 import pl.altkom.asc.lab.micronaut.poc.documents.api.DocumentsOperations
 import pl.altkom.asc.lab.micronaut.poc.documents.api.queries.finddocuments.FindDocumentsResult
 import pl.altkom.asc.lab.micronaut.poc.documents.api.queries.finddocuments.GeneratedDocument
 import pl.altkom.asc.lab.micronaut.poc.documents.domain.PolicyDocumentRepository
-import io.micronaut.http.annotation.Controller
-import io.micronaut.validation.Validated
 
+@ExecuteOn(TaskExecutors.IO)
 @Validated
 @Controller("/documents")
 class DocumentsController(private val policyDocumentRepository: PolicyDocumentRepository) : DocumentsOperations {
